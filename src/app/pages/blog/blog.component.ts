170bb1cd1a8269e49465d43328ba9ac4e5eb865b
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2025-08-23 07:22:44 +0300

    Fix duplicate FormsModule import

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
