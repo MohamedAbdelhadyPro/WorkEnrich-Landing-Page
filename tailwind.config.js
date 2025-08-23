441c0b4cad750425eb3d4b4367af75f5ccc88489
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2025-08-23 14:23:51 +0300

    إصلاح مشاكل التنسيق في الصفحة

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cairo': ['Cairo', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#1A3477',
+        'secondary': '#2563eb',
+        'orange': '#ff7000',
+      }
+    },
   },
   plugins: [],
 }
